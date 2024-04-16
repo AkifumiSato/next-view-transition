a8523219d463fa8fc3830cf714c7b849e71099c9
akfm.sato <01047245@CF0286.local>
2024-04-16 19:08:22 +0900

    feat: browser back transition with react transition promise

--- src/app/_components/back-forward-transition.tsx
+++ src/app/_components/back-forward-transition.tsx
@@ -1,33 +1,59 @@
+/// <reference types="navigation-api-types" />
 "use client";
-import { use, useEffect, useState, useTransition } from "react";
+import { use, useEffect, useRef, useState, useTransition } from "react";
 import { documentTransition } from "~/app/_lib/document-transition";
 
 export function BackForwardTransition() {
-  const [pending, startTransition] = useTransition();
-  const [promise, setPromise] = useState<Promise<void>>(null);
+  const [isPending, startTransition] = useTransition();
+  const reactTransitionPromise = useRef<PromiseWithResolvers<void>>();
+  const [viewTransitionPromise, setViewTransitionPromise] =
+    useState<Promise<void>>();
 
   useEffect(() => {
-    const ac = new AbortController();
-    const { signal } = ac;
+    if (!isPending && reactTransitionPromise.current) {
+      reactTransitionPromise.current.resolve();
+      reactTransitionPromise.current = undefined;
+    }
+  }, [isPending]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const { signal } = abortController;
+
+    window.navigation?.addEventListener(
+      "navigate",
+      ({ navigationType }) => {
+        if (navigationType !== "traverse") {
+          return;
+        }
+        // `isPending: true`でrenderし、`reactTransitionPromise`を解決させることで
+        // React TransitionのPromiseとして扱える
+        startTransition(() => {});
+        reactTransitionPromise.current = Promise.withResolvers();
+      },
+      { signal },
+    );
+
     window.addEventListener(
       "popstate",
       () => {
         startTransition(() => {
           const { promise, resolve } = Promise.withResolvers<void>();
-          setPromise(promise);
+          setViewTransitionPromise(promise);
           documentTransition(() => {
             resolve();
-            setPromise(null);
+            setViewTransitionPromise(undefined);
+            return reactTransitionPromise.current.promise;
           });
         });
       },
       { signal },
     );
-    return () => ac.abort();
+    return () => abortController.abort();
   }, []);
 
-  if (!pending && promise) {
-    use(promise);
+  if (!isPending && viewTransitionPromise) {
+    use(viewTransitionPromise);
   }
   return null;
 }
